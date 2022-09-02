90186801e12d36f80ccf9fa5d857ad1471a02f92
BMO | Viktor <veke2003@outlook.com>
2022-09-02 16:12:24 +0200

    sn py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame.locals import *
-
+import pygame.freetype
 
 pygame.init()
 #Define simple colors
@@ -13,15 +13,46 @@ Background = GREEN;
 screen.fill(Background)
 pygame.display.update()
 # Simple way of quitting an event loop
+
+#text - main menu
+def maintext(running, defaultText = 'Press "=" to run the game'):
+    if running == 1:
+        font = pygame.font.SysFont(None, 60)
+        img = font.render(defaultText, True, BLUE)
+        pygame.display.update()      
+        screen.blit(img, (500, 360))
+
+#player - controls | not yet done 
+def playerMovement():
+    if event.key == pygame.K_r:
+        Background = RED
+        screen.fill(Background)
+        pygame.display.update()
+    if event.key == pygame.K_g:
+        Background = GREEN
+        screen.fill(Background)
+        pygame.display.update()
+    if event.key == pygame.K_b:
+        Background = BLUE
+        screen.fill(Background)
+        pygame.display.update() 
+    
+
 running = True
 while running:
     for event in pygame.event.get():
+        maintext(1)
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_r:
-                Background = BLUE
-                screen.fill(Background)
+            #Player movement - controls
+            playerMovement()
+            if event.key == pygame.K_EQUALS:
                 pygame.display.update()
+                screen.fill(pygame.Color("black")) # erases the entire screen surface
+                maintext(0, 'Game is running')
+                screen.blit(screen, (0, 0))
+                print("Start")
+
 pygame.quit()
 
