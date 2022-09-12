b5ddfec55477b7d71a1393d1745380ed3ef5e4d1
BMO | Viktor <veke2003@outlook.com>
2022-09-12 13:02:14 +0200

    clean up code to prepare making snake ,add func.py

---
+++ func.py
@@ -0,0 +1,19 @@
+#All functions needed for the main program
+
+
+#player - controls | not yet done 
+# def playerMovement():
+    # if event.key == pygame.K_w:
+    #     screen.fill(bgColor)
+    #     pygame.display.update()
+    # if event.key == pygame.K_a:
+    #     screen.fill(bgColor)
+    #     pygame.display.update()
+    # if event.key == pygame.K_s:
+    #     screen.fill(bgColor)
+    #     pygame.display.update()
+    # if event.key == pygame.K_d:
+    #     screen.fill(bgColor)
+    #     pygame.display.update()
+
+    
--- main.py
+++ main.py
@@ -3,54 +3,27 @@ from pygame.locals import *
 import pygame.freetype
 
 pygame.init()
+
 #Define simple colors
 screen = pygame.display.set_mode((1280, 720));
-#set default bgColor
+
+#set default colors
 bgColor = (51, 51, 51)
 textColor = (255, 255, 255)
 
-# Simple way of quitting an event loop
+# default background
+screen.fill(bgColor)
+pygame.display.update()
 
-#text - main menu
-def maintext(running, defaultText = 'Press "=" to run the game'):
-    if running == 1:
-        font = pygame.font.SysFont(None, 60)
-        img = font.render(defaultText, True, textColor)
-        pygame.display.update()      
-        screen.blit(img, (500, 360))
-    else:
-        pygame.display.update()
-        screen.fill(bgColor)
-        pygame.display.update()
-
-#player - controls | not yet done 
-def playerMovement():
-    if event.key == pygame.K_w:
-        screen.fill(bgColor)
-        pygame.display.update()
-    if event.key == pygame.K_a:
-        screen.fill(bgColor)
-        pygame.display.update()
-    if event.key == pygame.K_s:
-        screen.fill(bgColor)
-        pygame.display.update()
-    if event.key == pygame.K_d:
-        screen.fill(bgColor)
-        pygame.display.update()
-    
-
-running = True
-while running:
+#game state
+game = 1
+while game:
     for event in pygame.event.get():
-        maintext(1)
         if event.type == pygame.QUIT:
-            running = False
+            game = 0
         if event.type == pygame.KEYDOWN:
             #Player movement - controls
-            playerMovement()
             if event.key == pygame.K_EQUALS:
-                maintext(0)
-                screen.fill(pygame.Color("black")) # erases the entire screen surface
-                maintext(0, 'Game is running')
+                print("pressed =")
 
 pygame.quit()
